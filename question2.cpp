26c799249f869742f19d60a3851b44daed3e2016
Aadil Sattar <a.r.sattar@student.reading.ac.uk>
2022-02-11 04:26:18 +0000

    added annotation question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -1,42 +1,62 @@
-#include<iostream>
+//includes all the libraries needed for the program to run
+#include<iostream> 
 #include <string>
 #include <sstream>
+
 using namespace std;
 
+//creates the base class known as calculator
 class calculator {
 public:
+
+    //adds floats known as num1 and num2 to be used in the other classes
     float num1;
     float num2;
     virtual float ans() { return 0; };
 };
+
+//creates derived class known as add 
 class add :public calculator {
 public:
+
+    //has function ans and executes it to add num1 and num2
     float ans() {
         return num1 + num2;
     }
 };
 
+//creates derived class known as sub 
 class sub :public calculator {
 public:
+
+    //has function ans and executes it to subtracts num2 from num1
     float ans() {
         return num1 - num2;
     }
 };
 
+//creates derived class known as mul 
 class mul :public calculator {
 public:
+
+    //has function ans and executes it to multiply num1 and num2 
     float ans() {
         return num1 * num2;
     }
 };
 
+//creates derived class known as div 
 class divi :public calculator {
 public:
+
+    //has function ans and executes it to divides num1 by num2
     float ans() {
         return num1 / num2;
     }
 };
 
+//sourced from "https://stackoverflow.com/questions/447206/c-isfloat-function/57163016"
+//this is the function to make sure a number is a float
 bool isFloat(string myString) {
     std::istringstream iss(myString);
     float f;
@@ -46,58 +66,74 @@ bool isFloat(string myString) {
 }
 
 float main() {
+
+    //converts the class' in to local variables to be used in main()
     calculator cal;
     add A;
     sub S;
     mul M;
     divi D;
-    string userinput;
+
+    //defines the variables needed in main()
     float num1;
     float num2;
+    string input;
     string choice;
 
+    //this takes the input for num1 as a string and makes sure it is a valid float
     while (true) {
-
+        
         cout << "Please give the first number: ";
-        getline(cin >> ws, userinput);
-        if (isFloat(userinput) == 0)
+        getline(cin >> ws, input);
+
+        //if it is not a float, it will loop after telling you it is invalid
+        if (isFloat(input) == 0)
         {
             cout << "Invalid, please insert a valid float \n";
         }
 
+        //if it is a valid float, it will change it to a float and break the loop
         else
         {
-            num1 = stof(userinput);
+            num1 = stof(input);
             break;
         }
     }
 
+    //this takes the input for num2 as a string and makes sure it is a valid float
     while (true)
     {
         cout << "Please give the second number: ";
-        getline(cin >> ws, userinput);
+        getline(cin >> ws, input);
 
-        if (isFloat(userinput) == 0)
+        //if it is not a float, it will loop after telling you it is invalid
+        if (isFloat(input) == 0)
         {
             cout << "Invalid, please insert a valid float \n";
         }
 
+        //if it is a valid float, it will change it to a float and break the loop
         else
         {
-            num2 = stof(userinput);
+            num2 = stof(input);
             break;
         }
     }
-
+    
+    //loops till a valid input is given for choice
     while (true) {
+        
+        //takes input of choice
         cout << "What kind of calculation would you like to do? (Add, Subtract, Multiply or Divide) ";
         cin.clear();
         getline(cin, choice);
 
+        //makes choice all lowercase, which stops any errors from not having proper case
         for (int i = 0; i < choice.length(); i++) {
             choice[i] = tolower(choice[i]);
         }
 
+        //if choice is add or plus(+), it passes the num1 and num2 to A to be used in the add class, before executing ans() function
         if (choice == "add" or choice == "+") {
             A.num1 = num1;
             A.num2 = num2;
@@ -106,6 +142,7 @@ float main() {
             break;
         }
 
+        //if choice is subtract or minus(-), it passes the num1 and num2 to S to be used in the sub class, before executing ans() function
         else if (choice == "substract" or choice == "-") {
             S.num1 = num1;
             S.num2 = num2;
@@ -114,6 +151,7 @@ float main() {
             break;
         }
 
+        //if choice is multiply or asterisks(*), it passes the num1 and num2 to M to be used in the mul class, before executing ans() function
         else if (choice == "multiply" or choice == "*") {
             M.num1 = num1;
             M.num2 = num2;
@@ -122,6 +160,7 @@ float main() {
             break;
         }
 
+        //if choice is divide or forward slash(/), it passes the num1 and num2 to D to be used in the divi class, before executing ans() function
         else if (choice == "divide" or choice == "/") {
             D.num1 = num1;
             D.num2 = num2;
@@ -130,6 +169,7 @@ float main() {
             break;
         }
 
+        //if choice is invalid, it repeats the loop till a valid choice is given by the user
         else {
             cout << "Please give a valid input from the options stated \n";
         }
